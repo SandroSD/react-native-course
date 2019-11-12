52d6a1aa9c9a673500ecc084ca67bd85fb391aa7
SandroSD <sdezerio@soulitsolutions.com>
2019-11-12 08:52:44 -0300

    Continuar desde el video 32

--- 02 - Diving into the Basics/rn-complete-guide/App.js
+++ 02 - Diving into the Basics/rn-complete-guide/App.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  return (
-	<View style={styles.screen}>
-		<View style={styles.inputContainer}>
-			<TextInput	placeholder="text"
-						style={styles.input}
-			/>
-			<Button title="ADD" />
-		</View>
-		<View>
+	const [courseGoals, setCourseGoals] = useState([]);
 
+	const addGoalHandler = goalTitle => {
+		setCourseGoals(currentGoals => [
+			...currentGoals,
+			{ key: Math.random().toString(), value: goalTitle }
+		 ]);
+	}
+
+	return (
+		<View style={styles.screen}>
+			<GoalInput	onAddGoal = {addGoalHandler} />
+			<FlatList
+				keyExtractor={(item, index) => item.id}
+				data={courseGoals}
+				renderItem={itemData => <GoalItem title={itemData.item.value} />
+			}/>
 		</View>
-	</View>
-  );
+	);
 }
 
 const styles = StyleSheet.create({
 	screen: {
 		padding: 30
-	},
-	inputContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center'
-	},
-	input: {
-		width: '80%',
-		borderBottomColor: 'black',
-		borderWidth: 1,
-		padding: 10
 	}
 });
